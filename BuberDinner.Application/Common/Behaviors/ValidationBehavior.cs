b34512388d9db4f6fce33aa65198793eb75e9f22
Uallessonivo <uallessons@gmail.com>
2023-02-23 11:37:02 -0300

    validation behavior with MediatR + FluentValidation

---
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using BuberDinner.Domain.Common.Errors;
+using FluentValidation;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Errors
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors.ConvertAll(failure =>
+            Errors.User.Validation(failure.PropertyName, failure.ErrorMessage));
+
+        return (dynamic)errors;
+    }
+}
--- BuberDinner.Domain/Common/Errors/Errors.User.cs
+++ BuberDinner.Domain/Common/Errors/Errors.User.cs
@@ -13,5 +13,8 @@ public class Errors
     {
         public static readonly Errors DuplicateEmail = new Errors("This email already exists.");
         public static readonly Errors InvalidEmailOrPassword = new Errors("Invalid email or password.");
+
+        public static Errors Validation(string propertyName, string errorMessage) =>
+            new Errors($"error: {propertyName}, message: {errorMessage}");
     }
 }
