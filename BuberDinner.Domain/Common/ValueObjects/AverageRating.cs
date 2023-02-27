2f9e05e9be476f59a9d981f9079167fe558533d3
Uallessonivo <uallessons@gmail.com>
2023-02-27 13:29:52 -0300

    domain layer structure and skeleton

---
+++ BuberDinner.Domain/Common/ValueObjects/AverageRating.cs
@@ -0,0 +1,35 @@
+﻿using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Common.ValueObjects;
+
+public sealed class AverageRating : ValueObject
+{
+    public AverageRating(double value, int numRating)
+    {
+        NumRating = numRating;
+        Value = value;
+    }
+
+    public double Value { get; private set; }
+    public int NumRating { get; private set; }
+
+    public static AverageRating CreateNew(double rating = 0, int numRating = 0)
+    {
+        return new AverageRating(rating, numRating);
+    }
+
+    public void AddNewRating(Rating rating)
+    {
+        Value = ((Value * NumRating) + rating.Value) / ++NumRating;
+    }
+    
+    public void RemoveRating(Rating rating)
+    {
+        Value = ((Value * NumRating) - rating.Value) / --NumRating;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
